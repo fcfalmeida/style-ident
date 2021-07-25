4adecf585b3c13e380c93767c8e0921e6aafa8e6
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-07-25 15:34:55 +0100

    add missing type annotations to FeatureExtractor

--- src/features/feature_extractor.py
+++ src/features/feature_extractor.py
@@ -1,6 +1,7 @@
+import pandas as pd
 from abc import ABC, abstractmethod
 
 class FeatureExtractor(ABC):
     @abstractmethod
-    def extract(data):
+    def extract(self, data: pd.DataFrame) -> pd.DataFrame:
         pass
