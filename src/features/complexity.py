fd4ea6be1ecd069ae0c2b988600fe614d044f87a
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-08-16 15:10:09 +0100

    force complexity features to return 0 for zero chroma vectors

--- src/features/complexity.py
+++ src/features/complexity.py
@@ -15,7 +15,8 @@ class Complexity(FeatureExtractor):
         return sorted
 
     def _sum_chroma_diff(self, chroma_vector: ArrayLike):
-        diff_sum = 0
+        zero_chromas = np.where(~chroma_vector.any(axis=1))[0]
+        diff_sum = np.zeros(chroma_vector.shape[0])
 
         for q in range(12):
             diff = chroma_vector[:, (q + 1) % 12] - chroma_vector[:, q]
@@ -23,16 +24,24 @@ class Complexity(FeatureExtractor):
 
             diff_sum += diff
 
-        return 1 - diff_sum / 2
+        diff_sum = 1 - diff_sum / 2
+        diff_sum.put(zero_chromas, 0)
+
+        return diff_sum
 
     def _chroma_std(self, chroma_vector: ArrayLike):
+        zero_chromas = np.where(~chroma_vector.any(axis=1))[0]
         std = np.std(chroma_vector, axis=1)
 
         rescale_factor = 1 / np.sqrt(12)
 
-        return 1 - std / rescale_factor
+        std = 1 - std / rescale_factor
+        std.put(zero_chromas, 0)
+
+        return std
 
     def _neg_slope(self, chroma_vector: ArrayLike):
+        zero_chromas = np.where(~chroma_vector.any(axis=1))[0]
         chroma_vector = np.sort(-chroma_vector, axis=1)
         pitch_classes = np.array(list(range(12)))
 
@@ -43,7 +52,10 @@ class Complexity(FeatureExtractor):
 
         rescale_factor = 0.039
 
-        return 1 - np.abs(slopes) / rescale_factor
+        slopes = 1 - np.abs(slopes) / rescale_factor
+        slopes.put(zero_chromas, 0)
+
+        return slopes
 
     def _entropy(self, chroma_vector: ArrayLike):
         entropies = -1 / np.log2(12) * np.sum(chroma_vector * np.log2(chroma_vector, where=chroma_vector > 0), axis=1)
@@ -51,29 +63,36 @@ class Complexity(FeatureExtractor):
         return entropies
 
     def _non_sparseness(self, chroma_vector: ArrayLike):
+        zero_chromas = np.where(~chroma_vector.any(axis=1))[0]
         l1 = math.l1_norm(chroma_vector)
         l2 = math.l2_norm(chroma_vector)
 
-        return 1 - (np.sqrt(12) - l1 / l2) / (np.sqrt(12) - 1)
+        non_sparse = 1 - (np.sqrt(12) - np.divide(l1, l2, where=l2 > 0)) / (np.sqrt(12) - 1)
+        non_sparse.put(zero_chromas, 0)
+
+        return non_sparse
 
     def _flatness(self, chroma_vector: ArrayLike):
-        geom_mean = gmean(chroma_vector, axis=1)
+        with np.errstate(divide='ignore'):
+            geom_mean = gmean(chroma_vector, axis=1)
+        
         arith_mean = np.mean(chroma_vector, axis=1)
 
-        return geom_mean / arith_mean
+        return np.divide(geom_mean, arith_mean, where=arith_mean > 0)
 
     def _angular_deviation(self, chroma_vector: ArrayLike):
+        zero_chromas = np.where(~chroma_vector.any(axis=1))[0]
         fifth_sorted = self._sort_chroma_fifths(chroma_vector)
-
         pitch_class_dist = 0
 
         for q in range(12):
-            temp = fifth_sorted[:, q] * np.exp(2j * np.pi * q / 12)
             pitch_class_dist += fifth_sorted[:, q] * np.exp(2j * np.pi * q / 12)
 
         pitch_class_dist = np.abs(pitch_class_dist)
+        pitch_class_dist = np.sqrt(1 - pitch_class_dist)
+        pitch_class_dist.put(zero_chromas, 0)
 
-        return np.sqrt(1 - pitch_class_dist)
+        return pitch_class_dist
 
     def extract(self, data: pd.DataFrame) -> pd.DataFrame:
         return super().extract(data)
--- tests/features/test_complexity.py
+++ tests/features/test_complexity.py
@@ -11,7 +11,9 @@ class TestComplexity:
             {'piece': 'bach_2.mp3', 'time': 0, 'c1': 0.03, 'c2': 0.07, 'c3': 0.11, 'c4': 0.09, 
                 'c5': 0.01, 'c6': 0.04, 'c7': 0.06, 'c8': 0.13, 'c9': 0.06, 'c10': 0.21, 'c11': 0.12, 'c12': 0.07},
             {'piece': 'bach_2.mp3', 'time': 0.1, 'c1': 0.02, 'c2': 0.08, 'c3': 0.14, 'c4': 0.05, 
-                'c5': 0.01, 'c6': 0.15, 'c7': 0.10, 'c8': 0.05, 'c9': 0.03, 'c10': 0.07, 'c11': 0.18, 'c12': 0.12}
+                'c5': 0.01, 'c6': 0.15, 'c7': 0.10, 'c8': 0.05, 'c9': 0.03, 'c10': 0.07, 'c11': 0.18, 'c12': 0.12},
+            {'piece': 'bach_2.mp3', 'time': 0.2, 'c1': 0, 'c2': 0, 'c3': 0, 'c4': 0, 
+                'c5': 0, 'c6': 0, 'c7': 0, 'c8': 0, 'c9': 0, 'c10': 0, 'c11': 0, 'c12': 0}
         ])
 
         df['time'] = pd.to_timedelta(df['time'], unit='s')
@@ -31,7 +33,8 @@ class TestComplexity:
     def test_sort_chroma_fifths(self, data):
         expected = np.array([
             [0.03, 0.13, 0.11, 0.21, 0.01, 0.07, 0.06, 0.07, 0.06, 0.09, 0.12, 0.04],
-            [0.02, 0.05, 0.14, 0.07, 0.01, 0.12, 0.1, 0.08, 0.03, 0.05, 0.18, 0.15]
+            [0.02, 0.05, 0.14, 0.07, 0.01, 0.12, 0.1, 0.08, 0.03, 0.05, 0.18, 0.15],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         ])
 
         result = Complexity()._sort_chroma_fifths(data[CHROMA_COLS].values)
@@ -39,7 +42,7 @@ class TestComplexity:
         assert np.array_equal(result, expected)
 
     def test_sum_chroma_diff(self, data, flat_vector, sparse_vector):
-        expected = np.array([0.65, 0.59])
+        expected = np.array([0.65, 0.59, 0])
 
         result = Complexity()._sum_chroma_diff(data[CHROMA_COLS].values)
         assert np.allclose(result, expected)
@@ -51,7 +54,7 @@ class TestComplexity:
         assert np.allclose(result, np.array([0]))
 
     def test_chroma_std(self, data, flat_vector, sparse_vector):
-        expected = np.array([0.82148763, 0.81760848])
+        expected = np.array([0.82148763, 0.81760848, 0])
 
         result = Complexity()._chroma_std(data[CHROMA_COLS].values)
         assert np.allclose(result, expected)
@@ -63,7 +66,7 @@ class TestComplexity:
         assert np.allclose(result, np.array([0.042572892]))
 
     def test_neg_slope(self, data, flat_vector, sparse_vector):
-        expected = np.array([0.641384257, 0.612694997])
+        expected = np.array([0.641384257, 0.612694997, 0])
 
         result = Complexity()._neg_slope(data[CHROMA_COLS].values)
         assert np.allclose(result, expected)
@@ -76,9 +79,10 @@ class TestComplexity:
 
 
     def test_entropy(self, data, flat_vector, sparse_vector):
-        expected = np.array([0.92430871434, 0.91369479299])
+        expected = np.array([0.92430871434, 0.91369479299, 0])
 
         result = Complexity()._entropy(data[CHROMA_COLS].values)
+        print(result)
         assert np.allclose(result, expected)
 
         result = Complexity()._entropy(flat_vector)
@@ -88,21 +92,23 @@ class TestComplexity:
         assert np.allclose(result, np.array([0]))
 
     def test_non_sparseness(self, data):
-        expected = np.array([0.78985308194, 0.78265321994])
+        expected = np.array([0.78985308194, 0.78265321994, 0])
 
         result = Complexity()._non_sparseness(data[CHROMA_COLS].values)
 
         assert np.allclose(result, expected)
 
     def test_flatness(self, data):
-        expected = np.array([0.7923240272, 0.754386555])
+        expected = np.array([0.7923240272, 0.754386555, 0])
 
         result = Complexity()._flatness(data[CHROMA_COLS].values)
+
+        print(result)
 
         assert np.allclose(result, expected)
 
     def test_angular_deviation(self, data, flat_vector, sparse_vector):
-        expected = np.array([0.9294068001, 0.955902087])
+        expected = np.array([0.9294068001, 0.955902087, 0])
 
         result = Complexity()._angular_deviation(data[CHROMA_COLS].values)
         assert np.allclose(result, expected)
