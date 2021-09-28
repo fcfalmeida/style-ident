24fbec836dea19d04c6e77dcde4234bfa4cf44a4
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-09-28 11:16:04 +0100

    implement HCDF

---
+++ src/features/hcdf.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+from numpy.typing import ArrayLike
+from scipy.ndimage.filters import gaussian_filter
+from TIVlib import TIV
+from src.data.pipeline_task import PipelineTask
+from src.data.constants import CHROMA_COLS
+from src.utils.math import cosine_distance
+
+
+class HCDF(PipelineTask):
+    DISTANCE_EUCLIDEAN = 0
+    DISTANCE_COSINE = 1
+
+    def __init__(
+        self,
+        gaussian_sigma: float = 5,
+        distance_metric: int = DISTANCE_EUCLIDEAN,
+        chroma_resolution: float = 0.1
+    ) -> None:
+        self.gaussian_sigma = gaussian_sigma
+        self.distance_metric = distance_metric
+        self.chroma_resolution = chroma_resolution
+
+    def _calc_tivs(self, chroma_vector: ArrayLike) -> ArrayLike:
+        # Supress 0 division warning in TIV calculation
+        with np.errstate(divide="ignore", invalid="ignore"):
+            tivs = np.apply_along_axis(
+                lambda c: TIV.from_pcp(c).get_vector(), 1, chroma_vector
+            )
+            tivs = np.nan_to_num(tivs)
+
+            return tivs
+
+    def _gaussian_blur(self, tivs: ArrayLike) -> ArrayLike:
+        blurred_tivs = gaussian_filter(
+            np.transpose(tivs), sigma=self.gaussian_sigma
+        )
+
+        return np.transpose(blurred_tivs)
+
+    def _calc_distance(
+        self, tivs1: ArrayLike, tivs2: ArrayLike = None
+    ) -> ArrayLike:
+        if tivs2 is None:
+            tivs2 = np.zeros_like(tivs1)
+
+        if self.distance_metric == self.DISTANCE_EUCLIDEAN:
+            try:
+                dist = np.linalg.norm(tivs1 - tivs2, axis=1)
+
+                return dist
+            except np.AxisError:
+                return np.linalg.norm(tivs1 - tivs2)
+        elif self.distance_metric == self.DISTANCE_COSINE:
+            return cosine_distance(tivs1, tivs2)
+
+    def _compute_hcdf(self, tivs: ArrayLike) -> ArrayLike:
+        hcdf = self._calc_distance(tivs[2:, :], tivs[:-2, :])
+
+        # Append HCDF values for the first and last TIVs
+        hcdf = np.insert(hcdf, 0, self._calc_distance(tivs[0]))
+        hcdf = np.append(hcdf, self._calc_distance(tivs[-1]))
+
+        # Map HCDF function as a (x, y) tuple where x represents time
+        # and y the value of the HCDF function at that time
+        hcdf = np.array([
+            (self.chroma_resolution * i, value) for i, value in enumerate(hcdf)
+        ])
+
+        return hcdf
+
+    def _extract_peaks(self, hcdf: ArrayLike) -> ArrayLike:
+        pass
+
+    def run(self, data: pd.DataFrame) -> pd.DataFrame:
+        tivs = self._calc_tivs(data[CHROMA_COLS].values)
+        smoothed_tivs = self._gaussian_blur(tivs)
+        hcdf = self._compute_hcdf(smoothed_tivs)
+
+        return hcdf
--- src/utils/math.py
+++ src/utils/math.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numpy.typing import ArrayLike
+from scipy.spatial import distance
 
 
 def l1_norm(vector: ArrayLike):
-    """Computes the l1 (Manhattan) norm of a vector
+    """Computes the l1 (Manhattan) norm of a vector.
 
     Args:
         vector: A vector or array of vectors.
@@ -15,7 +16,7 @@ def l1_norm(vector: ArrayLike):
 
 
 def l2_norm(vector: ArrayLike):
-    """Computes the l2 (Euclidian) norm of a vectoor
+    """Computes the l2 (Euclidean) norm of a vector.
 
     Args:
         vector: A vector or array of vectors.
@@ -24,3 +25,55 @@ def l2_norm(vector: ArrayLike):
         The l2 norm of the vector or an array of l2 norm value for each vector.
     """
     return np.sqrt(np.sum(np.power(vector, 2), axis=1))
+
+
+def dot_product(vector1: ArrayLike, vector2: ArrayLike) -> ArrayLike:
+    """Computes the dot product between two vectors.
+
+    If both vectors are 2-dimensional, it computes the dot product between
+    each row of `vector1` and `vector2`.
+
+    Args:
+        vector1: A vector or array of vectors.
+        vector2: Another vector or array of vectors.
+
+    Returns:
+        Dot product between `vector1` and `vector2.
+    """
+    try:
+        return np.sum(vector1 * vector2, axis=1)
+    except np.AxisError:
+        return np.sum(vector1 * vector2)
+
+
+def cosine_distance(vector1: ArrayLike, vector2: ArrayLike) -> float:
+    """Computes the cosine distance between two vectors or arrays of vectors.
+
+    Args:
+        vector1: A vector or array of vectors.
+        vector2: Another vector or array of vectors.
+
+    Returns:
+        Cosine distance between `vector1` and `vector2` (angle).
+    """
+
+    # If the dot product is a complex number, retrieve only its real part
+    dot = dot_product(vector1, vector2).real
+
+    try:
+        norm1 = np.linalg.norm(vector1, axis=1)
+        norm2 = np.linalg.norm(vector2, axis=1)
+    except np.AxisError:
+        norm1 = np.linalg.norm(vector1)
+        norm2 = np.linalg.norm(vector2)
+
+    # dist = np.arccos(dot / (norm1 * norm2))
+    norm_prod = norm1 * norm2
+    dist = np.arccos(
+        np.divide(dot, norm_prod, where=norm_prod != 0)
+    )
+
+    # Possible divison by zero above will return NaN
+    return np.nan_to_num(dist)
+
+    # return np.arccos(1 - distance.cosine(vector1, vector2))
