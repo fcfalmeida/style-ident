0d797016200ce485bd5feba335e23b6ee7a3d01b
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-10-10 13:01:43 +0100

    remove unused variable

--- src/data/make_hcdf_segmentation.py
+++ src/data/make_hcdf_segmentation.py
@@ -2,7 +2,6 @@ import click
 import pathlib
 import pandas as pd
 from src.data.pipeline import Pipeline
-from src.data.constants import CHROMA_COLS
 from src.features.hcdf_segmentation import HCDFSegmentation
 
 
@@ -10,9 +9,6 @@ from src.features.hcdf_segmentation import HCDFSegmentation
 @click.argument("input_filepath", type=click.Path(exists=True))
 @click.argument("output_filepath", type=click.Path())
 def main(input_filepath, output_filepath):
-    COL_NAMES = ["piece", "time"]
-    COL_NAMES.extend(CHROMA_COLS)
-
     for path in pathlib.Path(input_filepath).iterdir():
         if path.is_file():
             data = pd.read_csv(path, dtype={"piece": str})
