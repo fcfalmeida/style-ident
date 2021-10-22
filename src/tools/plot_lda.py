8857e6618c3e4b42f2b6eee2c95e620ea91b68d7
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-10-22 15:44:43 +0100

    add class names to lda plot legend

--- src/tools/plot_lda.py
+++ src/tools/plot_lda.py
@@ -18,18 +18,54 @@ def main(filepath):
 
     X_transformed = lda.fit_transform(X, y)
 
+    transformed_df = pd.DataFrame(
+        X_transformed, index=df.index, columns=['ld1', 'ld2']
+    )
+    transformed_df = _add_style_period_labels(transformed_df)
+
     plt.xlabel('Discriminant 1')
     plt.ylabel('Discriminant 2')
-    plt.scatter(
-        X_transformed[:, 0],
-        X_transformed[:, 1],
-        c=y,
-        cmap='rainbow',
-        alpha=0.7,
-        edgecolors='b'
+
+    baroque = transformed_df.loc[
+        transformed_df['style_period'] == 'baroque'
+    ]
+    classical = transformed_df.loc[
+        transformed_df['style_period'] == 'classical'
+    ]
+    romantic = transformed_df.loc[
+        transformed_df['style_period'] == 'romantic'
+    ]
+    modern = transformed_df.loc[
+        transformed_df['style_period'] == 'modern'
+    ]
+
+    alpha = 0.7
+    baroque_scatter = plt.scatter(
+        baroque['ld1'], baroque['ld2'], alpha=alpha
     )
-    plt.legend()
+    classical_scatter = plt.scatter(
+        classical['ld1'], classical['ld2'], alpha=alpha
+    )
+    romantic_scatter = plt.scatter(
+        romantic['ld1'], romantic['ld2'], alpha=alpha
+    )
+    modern_scatter = plt.scatter(
+        modern['ld1'], modern['ld2'], alpha=alpha
+    )
+
+    plt.legend(
+        (baroque_scatter, classical_scatter, romantic_scatter, modern_scatter),
+        ('baroque', 'classical', 'romantic', 'modern'),
+        fontsize=8
+    )
     plt.show()
+
+
+def _add_style_period_labels(data: pd.DataFrame) -> pd.DataFrame:
+    data["style_period"] = data.index.str.split("/").str[0]
+    data["style_period"] = data["style_period"].str.split("_").str[1]
+
+    return data
 
 
 if __name__ == '__main__':
