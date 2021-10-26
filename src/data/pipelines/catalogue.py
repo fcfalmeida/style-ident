b997fa559338dae293489180bba77237d8cb4472
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-10-26 17:28:34 +0100

    add Template Complexity pipeline

--- src/data/pipelines/catalogue.py
+++ src/data/pipelines/catalogue.py
@@ -2,10 +2,12 @@ from src.data.pipelines.tis_pipeline import TISPipeline
 from src.data.pipelines.tis_basic_pipeline import TISBasicPipeline
 from src.data.pipelines.tis_combined_pipeline import TISCombinedPipeline
 from src.data.pipelines.template_based_pipeline import TemplateBasedPipeline
+from src.data.pipelines.template_complexity import TemplateComplexityPipeline
 
 pipeline_catalogue = {
     'tis': TISPipeline(),
     'tis_basic': TISBasicPipeline(),
     'tis_combined': TISCombinedPipeline(),
-    'template_based': TemplateBasedPipeline
+    'template_based': TemplateBasedPipeline,
+    'template_complexity': TemplateComplexityPipeline
 }
