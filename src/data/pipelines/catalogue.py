bd54654319ba4dccf01ac68b23a7a2b7248457da
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-10-30 15:19:06 +0100

    add complexity pipeline

--- src/data/pipelines/catalogue.py
+++ src/data/pipelines/catalogue.py
@@ -3,11 +3,13 @@ from src.data.pipelines.tis_basic_pipeline import TISBasicPipeline
 from src.data.pipelines.tis_combined_pipeline import TISCombinedPipeline
 from src.data.pipelines.template_based_pipeline import TemplateBasedPipeline
 from src.data.pipelines.template_complexity import TemplateComplexityPipeline
+from src.data.pipelines.complexity_pipeline import ComplexityPipeline
 
 pipeline_catalogue = {
     'tis': TISPipeline(),
     'tis_basic': TISBasicPipeline(),
     'tis_combined': TISCombinedPipeline(),
+    'complexity': ComplexityPipeline,
     'template_based': TemplateBasedPipeline,
     'template_complexity': TemplateComplexityPipeline
 }
---
+++ src/data/pipelines/complexity_pipeline.py
@@ -0,0 +1,14 @@
+from src.data.pipelines.feature_pipeline import FeaturePipeline
+from src.data.constants.feature_groups import COMPLEXITY_FEATS
+from src.features.chroma_resolution import ChromaResolution
+from src.features.normalized_chroma import NormalizedChroma
+from src.features.complexity import Complexity
+
+
+class ComplexityPipeline(FeaturePipeline):
+    def __init__(self, chroma_resolution: float) -> None:
+        super().__init__(
+            [Complexity()],
+            COMPLEXITY_FEATS,
+            [ChromaResolution(chroma_resolution), NormalizedChroma()]
+        )
