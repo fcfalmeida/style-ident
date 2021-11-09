f4d80151a171aa2be497e31289a60037dfc0b33e
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-11-09 14:28:44 +0000

    add .csv output for training results

--- src/models/weiss.py
+++ src/models/weiss.py
@@ -2,10 +2,11 @@ import click
 import pathlib
 import pandas as pd
 import numpy as np
+import csv
 from sklearn import svm
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import cross_val_score, GridSearchCV, KFold
-from src.data.constants.others import PROCESSED_DIR
+from src.data.constants.others import PROCESSED_DIR, TRAINOUT_DIR
 from src.data.dataset_config import dataset_config
 
 
@@ -27,52 +28,66 @@ def execute(dataset: str, pipeline_name: str):
 
     for path in pathlib.Path(input_filepath).iterdir():
         if path.is_file():
-            print(f"Dataset -> {path.name}")
-            print("-" * 50)
 
-            data = pd.read_csv(path, dtype={"piece": str}, index_col="piece")
+            with open(
+                f'{TRAINOUT_DIR}/{path.stem}_trainout.csv', 'a', newline=''
+            ) as trainout_file:
+                writer = csv.writer(trainout_file)
 
-            le = LabelEncoder()
-            X = data.drop(target_col, axis=1).values
-            y = le.fit_transform(data[target_col])
+                print(f"Dataset -> {path.name}")
+                print("-" * 50)
 
-            c = [2 ** x for x in range(-5, 17, 2)]
-            gamma = [2 ** x for x in range(-15, 5, 2)]
+                data = pd.read_csv(
+                    path, dtype={"piece": str}, index_col="piece")
 
-            search_params = {
-                "C": c,
-                "gamma": gamma
-            }
-            svc = svm.SVC(kernel="rbf")
+                le = LabelEncoder()
+                X = data.drop(target_col, axis=1).values
+                y = le.fit_transform(data[target_col])
 
-            cv = KFold(n_splits=5, shuffle=True)
+                c = [2 ** x for x in range(-5, 17, 2)]
+                gamma = [2 ** x for x in range(-15, 5, 2)]
 
-            clf = GridSearchCV(svc, search_params, cv=cv)
-            clf.fit(X, y)
+                search_params = {
+                    "C": c,
+                    "gamma": gamma
+                }
+                svc = svm.SVC(kernel="rbf")
 
-            overall_mean_acc = 0
-            interfold_std = 0
-            mean_accuracies = []
-            runs = 10
-            for _ in range(runs):
-                mean, std = train(
-                    X, y, clf.best_params_["C"], clf.best_params_["gamma"]
-                )
+                cv = KFold(n_splits=5, shuffle=True)
 
-                overall_mean_acc += mean
-                mean_accuracies.append(mean)
-                interfold_std += std
+                clf = GridSearchCV(svc, search_params, cv=cv)
+                clf.fit(X, y)
 
-            overall_mean_acc /= runs
-            interfold_std /= runs
-            interrun_std = np.std(mean_accuracies)
+                overall_mean_acc = 0
+                interfold_std = 0
+                mean_accuracies = []
+                runs = 10
+                for _ in range(runs):
+                    mean, std = train(
+                        X, y, clf.best_params_["C"], clf.best_params_["gamma"]
+                    )
 
-            print(clf.best_params_)
+                    overall_mean_acc += mean
+                    mean_accuracies.append(mean)
+                    interfold_std += std
 
-            print(f"Mean Accuracy: {overall_mean_acc}")
-            print(f"Inter-run deviation: {interrun_std}")
-            print(f"Inter-fold deviation: {interfold_std}")
-            print("-" * 50)
+                overall_mean_acc /= runs
+                interfold_std /= runs
+                interrun_std = np.std(mean_accuracies)
+
+                print(clf.best_params_)
+
+                print(f"Mean Accuracy: {overall_mean_acc}")
+                print(f"Inter-run deviation: {interrun_std}")
+                print(f"Inter-fold deviation: {interfold_std}")
+                print("-" * 50)
+
+                writer.writerow([
+                    pipeline_name,
+                    overall_mean_acc,
+                    interrun_std,
+                    interfold_std
+                ])
 
     print('-' * 75)
     print('-' * 75)
