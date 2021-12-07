e81b19e12df0c2d278dbd63e535b7ee2388f1c59
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-12-07 12:14:43 +0000

    fix extract_chroma resolution

--- src/tools/extract_chroma.py
+++ src/tools/extract_chroma.py
@@ -10,10 +10,17 @@ from src.data.constants.feature_groups import CHROMA_FEATS
 @click.command()
 @click.argument('dataset', type=str)
 def main(dataset):
-    RESOLUTION = 0.1
-
-    input_filepath = f'{AUDIO_DIR}/{dataset}'
     output_filepath = f'{EXTERNAL_DIR}/{dataset}'
+
+    df = execute(dataset)
+
+    df.to_csv(f'{output_filepath}/{dataset}.csv')
+
+    print(df)
+
+
+def execute(dataset):
+    input_filepath = f'{AUDIO_DIR}/{dataset}'
 
     COL_NAMES = ['piece', 'time']
     COL_NAMES.extend(CHROMA_FEATS)
@@ -22,27 +29,37 @@ def main(dataset):
 
     for path in pathlib.Path(input_filepath).iterdir():
         if path.is_file():
-            y, sr = librosa.load(path)
-
-            chroma = _get_nnls(y, sr, 2048, 2048)
-
-            for i, chroma_vector in enumerate(chroma):
-                row = {'piece': path.stem, 'time': (i+1) * RESOLUTION}
-
-                for j, c in enumerate(chroma_vector):
-                    row[CHROMA_FEATS[j]] = c
-
-                rows.append(row)
+            piece_rows = extract(path)
+            rows.extend(piece_rows)
 
     df = pd.DataFrame(rows, columns=COL_NAMES)
     df = df.set_index(['piece', 'time'])
 
-    df.to_csv(f'{output_filepath}/{dataset}.csv')
-
-    print(df)
+    return df
 
 
-def _get_nnls(y, sr, fr, off):
+def extract(filepath):
+    y, sr = librosa.load(filepath)
+
+    chroma = _get_nnls(y, sr, 16384, 2048)
+
+    rows = []
+
+    for i, entry in enumerate(chroma):
+        time, chroma_vector = entry
+        # TODO: Fix resolution
+        # data = {'piece': filepath.stem, 'time': (i+1) * RESOLUTION}
+        data = {'piece': filepath.stem, 'time': float(time)}
+
+        for j, c in enumerate(chroma_vector):
+            data[CHROMA_FEATS[j]] = c
+
+        rows.append(data)
+
+    return rows
+
+
+def _get_nnls(samples, sr, block_size, off):
     """
         returns nnls chromagram
         Parameters
@@ -60,11 +77,19 @@ def _get_nnls(y, sr, fr, off):
         list of chromagrams
     """
     plugin = 'nnls-chroma:nnls-chroma'
-    chroma = list(vamp.process_audio(y, sr, plugin, output="chroma", block_size=fr, step_size=off))
+    chroma = list(vamp.process_audio(
+        samples,
+        sr,
+        plugin,
+        output='chroma',
+        block_size=block_size,
+        step_size=off
+    ))
     vectors = []
 
     for c in chroma:
-        vectors.append(c['values'].tolist())
+        chroma_vector = c['values'].tolist()
+        vectors.append((c['timestamp'], chroma_vector))
 
     return vectors
 
