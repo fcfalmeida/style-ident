bf15cec28cf240b25153ccbeea2e527bf14f3c14
Francisco Almeida <franciscocfalmeida@gmail.com>
2021-12-12 22:56:30 +0000

    make res an optional argument

--- src/tools/feature_boxplots.py
+++ src/tools/feature_boxplots.py
@@ -1,13 +1,8 @@
 import click
-import re
 import pandas as pd
 from matplotlib import pyplot as plt
-from TIVlib import TIVCollection
 from src.data.dataset_config import dataset_config
-from src.data.constants.others import EXTERNAL_DIR, PROCESSED_DIR
-from src.data.constants.features import TISFeats
-from src.data.constants.feature_groups import CHROMA_FEATS
-from src.features.tis_horizontal import TISHorizontal
+from src.data.constants.others import PROCESSED_DIR
 
 
 @click.command()
@@ -15,7 +10,7 @@ from src.features.tis_horizontal import TISHorizontal
 @click.argument('pipeline_name', type=str)
 @click.argument('feature', type=str)
 @click.argument('stat', type=str)
-@click.argument('res', type=str)
+@click.argument('res', type=str, required=False)
 def main(dataset, pipeline_name, feature, stat, res):
     input_filepath = f'{PROCESSED_DIR}/{dataset}/{pipeline_name}'
 
@@ -29,39 +24,25 @@ def main(dataset, pipeline_name, feature, stat, res):
     df = df.sort_values(by=[target_col, 'piece'])
     df = df.set_index('piece')
 
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
 
     ax.set_xticklabels(classes)
 
-    col_names = [col for col in df.columns if feature in col and stat in col and res in col]
+    col_names = [col for col in df.columns if feature in col and stat in col]
+
+    if res is not None:
+        col_names = [col for col in col_names if res in col]
 
     dfs_by_class = _split_dfs_by_class(df, target_col, classes)
 
     for c in col_names:
         y = []
-        for i, class_data in enumerate(dfs_by_class):
+        for class_data in dfs_by_class:
             y.append(class_data[c].values)
 
         ax.boxplot(y)
 
     plt.show()
-
-
-def _add_class_labels(data: pd.DataFrame, colname: str) -> pd.DataFrame:
-    data[colname] = data.index.str.split("/").str[0]
-    data[colname] = data[colname].str.split("_").str[-1]
-
-    return data
-
-
-def _make_xticks(count_per_class):
-    xticks = [0]
-
-    for i in range(len(count_per_class) - 1):
-        count = count_per_class[i]
-        xticks.append(xticks[i] + count)
-
-    return xticks
 
 
 def _split_dfs_by_class(df, target_col, classes):
@@ -72,12 +53,6 @@ def _split_dfs_by_class(df, target_col, classes):
         dfs_by_class.append(data)
 
     return dfs_by_class
-
-
-def _format_label(label):
-    res = re.search(r'\w+', label).group(0)
-
-    return res
 
 
 if __name__ == '__main__':
